4c5c644b49d7f5992b93cababcc0e27b35e5b17a
github username <github email address>
2018-05-26 00:47:56 -0600

    edited 5th method for .round

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -59,8 +59,8 @@ def get_average_age_for_season(data, season)
     count = count + item.to_i
   end
 
-  return_value = count.to_f / age_array.length
-  return_value.round
+  (return_value = count.to_f / age_array.length).round
+  return_value
 end
 
 #note
